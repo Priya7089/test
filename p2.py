56daa6924d57938a28308bac5066083015ad3c9d
Priya7089 <goswamikapil817@gmail.com>
2021-12-24 14:33:52 +0530

    v1

---
+++ p2.py
@@ -0,0 +1,13 @@
+students = ["archu","raghu","Chinu","Dadu","jyoti","khushi","priti","Harshu"] # LIST
+
+for student in students:
+    #print(f"Hello { student }, How are you ?")
+    print("Hello {}, How are you ?".format(student))
+
+students = ["archu","raghu","Chinu","Dadu","jyoti","khushi","priti","Harshu"] # LIST
+
+for student in students:
+    #print(f"Hello { student }, How are you ?")
+    print("Hello {}, How are you ?".format(student))
+
+
